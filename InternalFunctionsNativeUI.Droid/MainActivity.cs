ef61e4f386bab4e9047de72cc377b74b84bae830
jhonstupak <jhonstupak@gmail.com>
2019-01-21 21:35:03 +0300

    Add Drawer Layout

--- InternalFunctionsNativeUI.Droid/MainActivity.cs
+++ InternalFunctionsNativeUI.Droid/MainActivity.cs
@@ -2,18 +2,50 @@
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
-using Android.Widget;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+using System;
+using Android.Support.V7.Widget;
+using Android.Views;
 
 namespace InternalFunctionsNativeUI.Droid
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.Light", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private DrawerLayout _drawerLayout;
+        private NavigationView _navigationView;
+        private Toolbar _toolbar;
+        private ActionBarDrawerToggle _actionBarDrawerToggle;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.main_layout);
+
+            _toolbar = FindViewById<Toolbar>(Resource.Id.toolBar);
+            _drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            _navigationView = FindViewById<NavigationView>(Resource.Id.navigation_view);
+            _actionBarDrawerToggle = new ActionBarDrawerToggle(this, _drawerLayout, _toolbar, Resource.String.opend_drawer_message, Resource.String.closed_drawer_message);
+            _drawerLayout.AddDrawerListener(_actionBarDrawerToggle);
+            _actionBarDrawerToggle.SyncState();
+
+            SetSupportActionBar(_toolbar);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home)
+            {
+                _drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
